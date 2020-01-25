468ccc47a159dbb4d3a3b2a1254d27360d292c63
shimobayashi <akimasashimobayashi@gmail.com>
2020-01-25 19:24:44 +0900

    app.js内からapp.tsを実行するようにした。

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,33 +1,13 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+'use strict';
+// 以下の記事の内容を参考に動的にTypeScriptを実行している。
+// https://www.linkedin.com/pulse/another-possibility-typescript-aws-lambda-ruwan-geeganage
+// 手元でいちいちトランスパイルしなくて良いというだけで、実行効率は悪いと思われる。
+require('ts-node').register({
+  target: 'es6',
+  module: 'commonjs',
+  strict: true
+});
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+let app = require('./app.ts');
 
-    return response
-};
+module.exports.lambdaHandler = app.lambdaHandler.bind(app);
