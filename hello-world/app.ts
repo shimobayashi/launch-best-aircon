db7249ff27f07c1825140241839cf90ce1c4c6d0
shimobayashi <akimasashimobayashi@gmail.com>
2020-01-26 18:43:32 +0900

    室温に応じてエアコンを操作するようにした。

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -16,26 +16,36 @@ let response;
 export async function lambdaHandler (event, context) {
     try {
         // 現在の室温を取得する。
-        const now:number = Math.floor(Date.now() / 1000);
-        const ret:AxiosResponse = await axios({
-            url: 'https://api.mackerelio.com/api/v0/services/home/metrics',
-            params: {
-                name: 'natureremo.temperature.Remo',
-                // Mackerel内部では300秒ごとに値を保存しているようで、最新の値以外は要らないのでfromには300秒前を指定。
-                from: now - 300,
-                to:   now,
-            },
-            headers: {'X-Api-Key': process.env.MACKEREL_API_KEY},
-        } as AxiosRequestConfig);
-        const currentTemperature:number = ret.data['metrics'].slice(-1)[0]['value'];
+        let currentTemperature:number;
+        fetchCurrentTemperature: {
+            const now:number = Math.floor(Date.now() / 1000);
+            const ret:AxiosResponse = await axios({
+                url: 'https://api.mackerelio.com/api/v0/services/home/metrics',
+                params: {
+                    name: 'natureremo.temperature.Remo',
+                    // Mackerel内部では300秒ごとに値を保存しているようで、最新の値以外は要らないのでfromには300秒前を指定。
+                    from: now - 300,
+                    to:   now,
+                },
+                headers: {'X-Api-Key': process.env.MACKEREL_API_KEY},
+            } as AxiosRequestConfig);
+            currentTemperature = ret.data['metrics'].slice(-1)[0]['value'];
+        }
 
         // 室温に応じて適切にエアコンを操作する。
+        let message:string = 'Nothing to do';
+        if (currentTemperature < 18.0) {
+            const ret:AxiosResponse = await axios(`https://maker.ifttt.com/trigger/aircon_on_heater/with/key/${process.env.IFTTT_API_KEY}`);
+            message = ret.data.trim();
+        } else if (currentTemperature >= 30.0) {
+            const ret:AxiosResponse = await axios(`https://maker.ifttt.com/trigger/aircon_on_cooler/with/key/${process.env.IFTTT_API_KEY}`);
+            message = ret.data.trim();
+        }
 
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
+                message: message,
             })
         }
     } catch (err) {
